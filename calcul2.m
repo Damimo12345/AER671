7906de922690d3a46972b0d9bacdf483b579cf2f
Damimo12345 <150942757+Damimo12345@users.noreply.github.com>
2025-07-01 09:56:51 -0400

    New calcul 2

--- calcul2.m
+++ calcul2.m
@@ -1,27 +1,50 @@
 
-Hauteur = 25000 ; %On peux le modifier de 25000 ft à 29000 ft
+Hauteur = 29000 ; %On peux le modifier de 25000 ft à 29000 ft
 rho = density(Hauteur) ;
 
-v_initial = 265*1.687811; %en ft/sec pour le calcul de W/s cruise pour avoir le vrai V
+v_initial = 245*1.687811; %en ft/sec pour le calcul de W/s cruise pour avoir le vrai V
 M = v_initial / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8); % Calculate Mach number
 
 
-C_d0 = 0.020 ; %Le range change BCP quand je change le C_d0
-e = 0.78 ;
-A = 6;
+C_d0 = 0.022 ; %Le range change BCP quand je change le C_d0
+e = 0.8 ;
+A = 4;
 k = 1/(pi*A*e) ;
-
 
 reserve_fuel = 0.05 ;
 trapped_fuel= 0.01;
 W_payload = 500 + 2*180 ; %en lbs
 
-ws_cruising = ws_cruise(C_d0,A,Hauteur,M,'prop');
+
+
+ws_cruising = (rho*(v_initial^2)/2)*sqrt(C_d0/k);
 V = sqrt((2/rho)*ws_cruising)*((k/C_d0)^0.25); %ft/s
 V_kts = V/1.687811; % Vitesse en kts
 
+
+%%%%% Iteration sur la vitesse %%%%%
+
+% v_initial = 0 ;
+% V_kts = 265 ; % vitesse en KTS
+% 
+% while (abs(v_initial-V_kts) > 0.01)
+% 
+%     v_initial = V_kts ;
+% 
+%     v_initial_1 = v_initial*1.687811; %en ft/sec pour le calcul de W/s cruise pour avoir le vrai V
+%     M = v_initial_1 / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8); % Calculate Mach number
+% 
+%     ws_cruising = (rho*(v_initial_1^2)/2)*sqrt(C_d0/k);
+% 
+%     V = sqrt((2/rho)*ws_cruising)*((k/C_d0)^0.25); %ft/s
+%     V_kts = V/1.687811; % Vitesse en kts
+% 
+%     display(V_kts)
+% 
+% end
+
 L_D_max = sqrt(1/(4*C_d0*k));
-C_BHP=0.45;
+C_BHP=0.47;
 rendement = 0.8 ;
 C = consommation_spec(C_BHP,V,rendement);
 
@@ -30,11 +53,18 @@ C = consommation_spec(C_BHP,V,rendement);
 Range_start= 0; %nm commencer à 600nm
 Range_iteration = 600 ;
 
-while (abs(Range_start-Range_iteration) > 0.1) 
+Vitesse = 0 ;
+
+while (abs(Range_start-Range_iteration) > 0.1) && (abs(Vitesse-V) > 0.1) 
+
+    Vitesse = V ;
+
+    M = Vitesse / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8);
+    C = consommation_spec(C_BHP,Vitesse,rendement);
     
     Range_start = Range_iteration ;
 
-    [W_TO,W_fuel,W_empty] = itertow('aviation-gen(1mot)',M,Hauteur,A,C,0,reserve_fuel,trapped_fuel,W_payload,Range_start); %Aviation-gen(1mot) Je suis pas sûre que ça soit ça (on a prop 1 mot, mais il donne slm prop 2moteurs)
+    [W_TO,W_fuel,W_empty] = itertow('Aviation-gen(1mot)',M,Hauteur,A,C,0,reserve_fuel,trapped_fuel,W_payload,Range_start); %Aviation-gen(1mot) Je suis pas sûre que ça soit ça (on a prop 1 mot, mais il donne slm prop 2moteurs)
     
     %CALCULS POUR ITÉRATION SUR R (on recalcule R avec W_to et W_fuel)
     
@@ -52,15 +82,21 @@ while (abs(Range_start-Range_iteration) > 0.1)
     M = v_initial / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8);
     
     Range_iteration = (V_kts/C)*L_D_max*log(W2_W3); %Ln c'est log sur MatLab
+
+
+    %%%%%%   TROUVER S %%%%%%%
+    
+    S = W_TO/ws_cruising; %en FT^2 , À MODIFIER
+    
+    % Vérification pour le Distance de décollage et d'atterissage;
+    %Est-que on peux juste prendre le W_to final avec S qu' en prenant une
+    %valeur historique pour T/W
+
+    C_l_md = sqrt(C_d0/k);
+    Vitesse = sqrt(2*W_TO/(rho*S*C_l_md));
+
 end
 
-%%%%%%   TROUVER S %%%%%%%
-
-S = W_TO/ws_cruising; %en FT^2 , À MODIFIER
-
-% Vérification pour le Distance de décollage et d'atterissage;
-%Est-que on peux juste prendre le W_to final avec S qu' en prenant une
-%valeur historique pour T/W
 
 rhosl=density(0);
 
@@ -82,7 +118,7 @@ display(takeoffDistance)
 
 %%%%%%%%%%%%%%% LANDING DISTANCE %%%%%%%%%%%%%%%%
 
-C_l_LAmax = 2.7 ; % AVEC RAYMER
+C_l_LAmax = 2.3 ; % AVEC RAYMER
 % S_L_start = 2800; % ft On choisi au pif !!!
 
 W_S_LA = (W_TO-W_fuel+((reserve_fuel+trapped_fuel)*W_fuel))/S;
@@ -100,12 +136,10 @@ display(landingDistance)
 display(rho)
 display(V)
 display(W2_W3)
-display(V_kts)
 display(M)
 display(C)
 display(L_D_max)
 display(S)
-display(S2)
 display(Range_iteration)
 display(W_TO)
-display(ws_cruising)
+display(V_kts)
