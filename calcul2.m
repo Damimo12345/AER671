6bcbe72d3411b3cef86758be98f66a79892b15e5
Damimo12345 <150942757+Damimo12345@users.noreply.github.com>
2025-07-01 21:49:38 -0400

    Nouveau #2

--- calcul2.m
+++ calcul2.m
@@ -2,20 +2,18 @@
 Hauteur = 29000 ; %On peux le modifier de 25000 ft à 29000 ft
 rho = density(Hauteur) ;
 
-v_initial = 245*1.687811; %en ft/sec pour le calcul de W/s cruise pour avoir le vrai V
+v_initial = 145*1.687811; %en ft/sec pour le calcul de W/s cruise pour avoir le vrai V
 M = v_initial / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8); % Calculate Mach number
 
 
-C_d0 = 0.022 ; %Le range change BCP quand je change le C_d0
+C_d0 = 0.023 ; %Le range change BCP quand je change le C_d0
 e = 0.8 ;
-A = 4;
+A = 7;
 k = 1/(pi*A*e) ;
 
 reserve_fuel = 0.05 ;
 trapped_fuel= 0.01;
 W_payload = 500 + 2*180 ; %en lbs
-
-
 
 ws_cruising = (rho*(v_initial^2)/2)*sqrt(C_d0/k);
 V = sqrt((2/rho)*ws_cruising)*((k/C_d0)^0.25); %ft/s
@@ -55,13 +53,8 @@ Range_iteration = 600 ;
 
 Vitesse = 0 ;
 
-while (abs(Range_start-Range_iteration) > 0.1) && (abs(Vitesse-V) > 0.1) 
+while (abs(Range_start-Range_iteration) > 0.1)
 
-    Vitesse = V ;
-
-    M = Vitesse / sqrt(1.4 * 1716 * tempatmstd(Hauteur)*1.8);
-    C = consommation_spec(C_BHP,Vitesse,rendement);
-    
     Range_start = Range_iteration ;
 
     [W_TO,W_fuel,W_empty] = itertow('Aviation-gen(1mot)',M,Hauteur,A,C,0,reserve_fuel,trapped_fuel,W_payload,Range_start); %Aviation-gen(1mot) Je suis pas sûre que ça soit ça (on a prop 1 mot, mais il donne slm prop 2moteurs)
@@ -83,19 +76,15 @@ while (abs(Range_start-Range_iteration) > 0.1) && (abs(Vitesse-V) > 0.1)
     
     Range_iteration = (V_kts/C)*L_D_max*log(W2_W3); %Ln c'est log sur MatLab
 
-
-    %%%%%%   TROUVER S %%%%%%%
-    
-    S = W_TO/ws_cruising; %en FT^2 , À MODIFIER
-    
-    % Vérification pour le Distance de décollage et d'atterissage;
-    %Est-que on peux juste prendre le W_to final avec S qu' en prenant une
-    %valeur historique pour T/W
-
-    C_l_md = sqrt(C_d0/k);
-    Vitesse = sqrt(2*W_TO/(rho*S*C_l_md));
-
 end
+
+%%%%%%   TROUVER S %%%%%%%
+    
+S = W_TO/ws_cruising; %en FT^2 , À MODIFIER
+    
+% Vérification pour le Distance de décollage et d'atterissage;
+%Est-que on peux juste prendre le W_to final avec S qu' en prenant une
+%valeur historique pour T/W
 
 
 rhosl=density(0);
@@ -104,7 +93,7 @@ sigma = rho/rhosl;
 
 %%%%%%%%%%%%%%% TAKE OFF DISTANCE %%%%%%%%%%%%%%%%
 
-% s_TO_start = 2500 ; % ft, on choisi au pif !!!!!
+% s_TO_start = 2800 ; % ft, on choisi au pif !!!!!
 
 C_l_TOmax = 1.7 ; % AVEC RAYMER
 T_W = 0.5 ; %Donnée historique (0,25-0,57)
@@ -119,7 +108,7 @@ display(takeoffDistance)
 %%%%%%%%%%%%%%% LANDING DISTANCE %%%%%%%%%%%%%%%%
 
 C_l_LAmax = 2.3 ; % AVEC RAYMER
-% S_L_start = 2800; % ft On choisi au pif !!!
+% S_L_start = 3000; % ft On choisi au pif !!!
 
 W_S_LA = (W_TO-W_fuel+((reserve_fuel+trapped_fuel)*W_fuel))/S;
 LP = W_S_LA*(1/(sigma*C_l_LAmax)); 
@@ -128,8 +117,9 @@ landingDistance = 118*LP+400; %Pour vérifier
 
 display(landingDistance)
 
-
-
+V_s = sqrt(ws_cruising*2/(rho*C_l_LAmax));
+Puissance_disponible = T_W * V_s*1.1*0.7 ;
+P_hp = Puissance_disponible/(550*rendement*sigma);
 
 % Juste pour voir les valeurs
 
